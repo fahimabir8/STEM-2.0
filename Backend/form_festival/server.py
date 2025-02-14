7bdfb7bb7e0ea3b92f9a18205162609c9386004e
fahimabir8 <fahimabir8@users.noreply.github.com>
2025-02-14 20:41:08 +0600

    vercel recreated

---
+++ Backend/form_festival/server.py
@@ -0,0 +1,3 @@
+from form_festival.wsgi import application
+
+app = application
--- Backend/form_festival/wsgi.py
+++ Backend/form_festival/wsgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "form_festival.settings")
+
 application = get_wsgi_application()
-app = application
